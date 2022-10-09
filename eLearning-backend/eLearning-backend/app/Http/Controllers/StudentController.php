ddd586a5b52d4a4730028f63ed246d82897c14d6
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-09 20:36:01 +0300

    Backend: Student view Assignment edited

--- eLearning-backend/eLearning-backend/app/Http/Controllers/StudentController.php
+++ eLearning-backend/eLearning-backend/app/Http/Controllers/StudentController.php
@@ -5,26 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Assignment;
+use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
     // Student should view only assignmnets for courses relevant to his
     public function viewAssignment($id = null)
     {
-        $data = 'No Data';
-        $status = 'No Assignments Found';
+        $currUser = Auth::user();
 
-        if (!$id) {
-            $data =  Assignment::all();
-            $status = "Returning All";
-        } else {
-            $data = Assignment::find($id);
-            $status = "Returning ID " . $id;
+        if ($currUser['userType'] == 3) {
+            $data = 'No Data';
+            $status = 'No Assignments Found';
+
+            if (!$id) {
+                $data =  Assignment::all();
+                $status = "Returning All";
+            } else {
+                $data = Assignment::find($id);
+                $status = "Returning ID " . $id;
+            }
+
+            return response()->json([
+                "Status" => $status,
+                "Data" => $data
+            ]);
         }
 
         return response()->json([
-            "Status" => $status,
-            "Data" => $data
+            "Status" => 'Error'
         ]);
     }
 
