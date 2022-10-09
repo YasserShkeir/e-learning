9a4ee2be8c9a501b1af16fde5562969aa525745f
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-09 23:52:34 +0300

    Frontend: SignIn Initial Form Styling

--- eLearning-frontend/elearning-frontend/src/pages/SignIn.js
+++ eLearning-frontend/elearning-frontend/src/pages/SignIn.js
@@ -1,8 +1,32 @@
 import { useState, useEffect, React } from "react";
 import classes from "./SignIn.module.css";
+import StateButton from "../components/signIn/stateButton";
 
 function SignInPage() {
-  return <div>Hello</div>;
+  const [selectState, setSelectState] = useState(false);
+
+  const displaySignIn = () => {
+    if (!selectState) {
+      setSelectState(!selectState);
+      console.log(selectState);
+    }
+  };
+
+  const displaySignUp = () => {
+    if (selectState) {
+      setSelectState(!selectState);
+      console.log(selectState);
+    }
+  };
+
+  return (
+    <div className={classes.mainForm}>
+      <div className={classes.spaceBetween}>
+        <StateButton text={"Sign In"} onClick={displaySignIn} />
+        <StateButton text={"Sign Up"} onClick={displaySignUp} />
+      </div>
+    </div>
+  );
 }
 
 export default SignInPage;
