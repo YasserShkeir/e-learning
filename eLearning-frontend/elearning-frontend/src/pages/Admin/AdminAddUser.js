5f2a792043986811fa70debae0e1e9c7f64916d8
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-11 21:14:08 +0300

    Frontend: Admin AddUser Initial Done All Users

--- eLearning-frontend/elearning-frontend/src/pages/Admin/AdminAddUser.js
+++ eLearning-frontend/elearning-frontend/src/pages/Admin/AdminAddUser.js
@@ -4,28 +4,42 @@ import StateButton from "../../components/signIn/stateButton";
 import axios from "axios";
 
 const AdminAddUser = (option) => {
-  const addUserCaller = async (userType) => {
+  const addUserCaller = async () => {
     let data = {
       name: document.getElementById("addUserName").value,
       email: document.getElementById("addUserEmail").value,
-      password: document.getElementById("addUserEmail").value,
-      userType: userType,
+      password: document.getElementById("addUserPass").value,
+      userType: option.option,
     };
 
-    const res = await axios.post(
-      "http://127.0.0.1:8000/api/addUser",
-      {
-        name: document.getElementById("addUserName").value,
-        email: document.getElementById("addUserEmail").value,
-        password: document.getElementById("addUserEmail").value,
-        userType: option.option,
-      },
-      {
+    if (option.option == 2) {
+      data["courses"] = [document.getElementById("addInstructorCourses").value];
+    }
+
+    if (option.option == 3) {
+      data["enrolledCourses"] = [
+        document.getElementById("addStudentCourses").value,
+      ];
+      data["assignments"] = [
+        document.getElementById("addStudentAssignments").value,
+      ];
+    }
+
+    console.log();
+
+    const res = await axios
+      .post("http://127.0.0.1:8000/api/addUser", data, {
         headers: {
+          ContentType: "application/x-www-form-urlencoded;charset=UTF-8",
           Authorization: "Bearer " + localStorage.getItem("jwt"),
         },
-      }
-    );
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   //
@@ -52,7 +66,16 @@ const AdminAddUser = (option) => {
       return (
         <>
           <h3>Add Instructor</h3>
-          {commonForm()}
+          <div className={classes.adminInput}>
+            {commonForm()}
+            <div className={classes.flexStart}>
+              <FormRow
+                title="Courses"
+                inpType="text"
+                inpName="addInstructorCourses"
+              />
+            </div>
+          </div>
           <StateButton text={"Add User"} onClick={addUserCaller} />
         </>
       );
@@ -61,7 +84,22 @@ const AdminAddUser = (option) => {
       return (
         <>
           <h3>Add Student</h3>
-          {commonForm()}
+          <div className={classes.adminInput}>
+            {commonForm()}
+            <div className={classes.flexStart}>
+              <FormRow
+                title="Enrolled Courses"
+                inpType="text"
+                inpName="addStudentCourses"
+              />
+              <FormRow
+                title="Assignments"
+                inpType="text"
+                inpName="addStudentAssignments"
+              />
+            </div>
+          </div>
+          <StateButton text={"Add User"} onClick={addUserCaller} />
         </>
       );
     }
