8c358dcedf29f0a8d164b36af1b3ead728dff6fd
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-11 23:46:24 +0300

    Backend: General Tracking Commentation

--- eLearning-backend/eLearning-backend/routes/api.php
+++ eLearning-backend/eLearning-backend/routes/api.php
@@ -8,27 +8,28 @@ use App\Http\Controllers\InstructorController;
 use App\Http\Controllers\StudentController;
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-    Route::get('getSelf', 'getSelf');
+    Route::post('login', 'login'); // Done
+    Route::post('register', 'register'); // Done
+    Route::post('logout', 'logout'); // Done
+    Route::post('refresh', 'refresh'); // Done
+    Route::get('getSelf', 'getSelf'); // Done
 });
 
 
 
 Route::group(["middleware" => "auth:api"], function () {
+    // Admin APIs All Done
     Route::controller(AdminController::class)->group(function () {
-        Route::post('getsorted', 'getSortedUsers');
-        Route::post('addUser', 'addUser');
-        Route::get('/getUsers/{id?}', 'getUsers');
-        Route::get('/getCourses/{id?}', 'getCourses');
-        Route::post('addCourse', 'addCourse');
+        Route::post('getsorted', 'getSortedUsers'); // Done
+        Route::post('addUser', 'addUser'); // Done
+        Route::get('/getUsers/{id?}', 'getUsers'); // Done
+        Route::get('/getCourses/{id?}', 'getCourses'); // Done
+        Route::post('addCourse', 'addCourse'); // Done
     });
 
     Route::controller(InstructorController::class)->group(function () {
-        Route::post('addStudent', 'addStudent');
-        Route::post('updateStudentCourses', 'updateStudentCourses');
+        Route::post('addStudent', 'addStudent'); // Done
+        Route::post('updateStudentCourses', 'updateStudentCourses'); // Done
         Route::post('createAssignment', 'createAssignment');
         Route::post('createAnnouncement', 'createAnnouncement');
     });
